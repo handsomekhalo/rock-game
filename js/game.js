48c700410bb02fdc75fa77af0478288f06b23e8a
handsomekhalo <57897203+handsomekhalo@users.noreply.github.com>
2022-02-23 15:54:38 +0200

    full game

--- js/game.js
+++ js/game.js
@@ -1,56 +1,34 @@
 const choices = ["rock", "paper", "scissors"];
 const winners = [];
 
+
+function resetGame()
+{
+
+
+}
+
+
 function computerPlay() {
   //play the game
   //paly five rounds
-  //game is cnsole based
 
-  for (let x = 0; x < 5; x++) {
-    playRound(x);
-  }
-
-  logWinners();
 }
 
 function playRound(roundNum) {
-  const player = playerChoice();
   const computer = computerSelected();
   const winner = checkWinner(player, computer);
   winners.push(winner);
-  document.querySelector("button").textContent = "Start a new game"
-  logRounds(player, computer , winner, roundNum);
 
 }
 
-function playerChoice() {
-  // get playerinput
 
-  let input = prompt("Enter Rock , Paper , or Scissors");
 
-  while (input == null) {
-    input = prompt("Enter Rock , Paper , or Scissors");
-  }
-  input = input.toLowerCase();
-
-  let check = validateEntered(input);
-  while (check == false) {
-    input = prompt(
-      "Please enter correct capitalization wehnentreting rock , paper or scissors"
-    );
-    while (input == null) {
-      input = prompt("Enter Rock , Paper , or Scissors");
-    }
-    input = input.toLowerCase();
-    check = validateEntered(input);
-  }
-  return input;
-}
-
-function validateEntered(choice) {
-  return choices.includes(choice);
-}
 function computerSelected() {
+
+  //do dom here
+
+
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
@@ -76,18 +54,7 @@ function logWinners() {
   let compWins = winners.filter((item) => item == "computer").length;
   let ties = winners.filter((item) => item == "Tie").length;
 
-  console.log("Results:");
-  console.log("Player wins:", playerWins);
-
-  console.log("Computer wins:", compWins);
-
-  console.log("Its a Tie:", ties);
 }
 
-function logRounds(PlayersChoice, ComputersChoice, winnerIs, roundNum) {
-  console.log("Round:", roundNum);
-  console.log("player choice:", PlayersChoice);
-  console.log("Computer choice:", ComputersChoice);
-  console.log(winnerIs, "won this round");
-  console.log("-------------------------------------------")
-}
+computerPlay();
+
